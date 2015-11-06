6f84f04b96f2c2c0810d558e638bab3a142dcc32
Carmen Popoviciu <carmen.popoviciu@gmail.com>
2015-11-06 11:52:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## The magic of Web Components
+
+Slides for the [Domcode](http://conference.domcode.org/) talk about Web Components I gave with
+[Remco Gubbels](https://twitter.com/RemcoGubbels)
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require("gulp-open");
+
+var PORT_NUMBER = 9003;
+
+gulp.task('html', function () {
+    gulp.src('./slides/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task("open", function(){
+    gulp.src("./slides/index.html")
+        .pipe(open("", {
+        url: "http://localhost:" + PORT_NUMBER,
+        app: "google chrome"
+    }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./slides/*.html'], ['html']);
+});
+
+
+gulp.task('connect', function() {
+    connect.server({
+        root: '.',
+        port: PORT_NUMBER,
+        livereload: true
+    });
+});
+
+gulp.task('copy', function() {
+    gulp.src("slides/**/*")
+        .pipe(gulp.dest('./slides/public'));
+});
+
+// default task
+gulp.task('default', ['open', 'connect', 'watch']);
+
